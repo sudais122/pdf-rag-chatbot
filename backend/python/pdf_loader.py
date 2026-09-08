4cc747d4216b6a9d92bb5468055091845ffc3063
sudais122 <khansb17798@gmail.com>
2026-09-08 22:00:47 +0500

    feat:pyhton code complete

--- backend/python/pdf_loader.py
+++ backend/python/pdf_loader.py
@@ -1,4 +1,6 @@
 from pypdf import PdfReader
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
 reader = PdfReader("../documents/example.pdf")
 
@@ -9,6 +11,39 @@ for page in reader.pages:
 
 chunks = text.split("\n\n")
 
-for i, chunk in enumerate(chunks):
-    print(f"\n--- Chunk {i} ---")
-    print(chunk)
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+chunks_embeddings = model.encode(chunks)
+
+query = input("Ask anything: ")
+
+query_embedding = model.encode([query])
+
+print("Number of chunks:", len(chunks))
+print("Embedding size:", len(chunks_embeddings[0]))
+
+score = cosine_similarity(query_embedding, chunks_embeddings)
+
+top_k = 3
+
+top_indices = score[0].argsort()[-top_k:][::-1]
+
+retrieved_chunks = [chunks[i] for i in top_indices]
+
+context = "\n\n".join(retrieved_chunks)
+
+print("\n--- Retrieved Context ---")
+print(context)
+
+prompt = f"""
+Answer the question using only the following context.
+
+Context:
+{context}
+
+Question:
+{query}
+"""
+
+print("\n--- Prompt ---")
+print(prompt)
